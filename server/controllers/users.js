06fa2a857d9f85df91bc3fe279417272e5f26c3f
Eren Derman <can.derman@staj.turkcell.com.tr>
2018-03-07 15:19:05 -0500

    heroku dev2

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -8,7 +8,8 @@ module.exports = {
         title: req.body.title,
       })
       .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
+      .catch(error => res.status(400).send(error)
+        console.log(error));
   },
   list(req, res) {
   return User
