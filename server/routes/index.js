3de568f4e6ceccdc64c0c6d824f631f5153d933c
Eren Derman <can.derman@staj.turkcell.com.tr>
2018-04-15 17:25:11 -0400

    mal muratttt

--- server/routes/index.js
+++ server/routes/index.js
@@ -23,7 +23,7 @@ module.exports = (app) => {
   app.get('/api/users', usersController.list);
   app.get('/api/users/:userId', usersController.retrieve);
 
-  app.get('/api/calendar', function(req, res){
+  app.get('/api/calendar/pool', function(req, res){
     // Load client secrets from a local file.
       content = {"installed":{"client_id":"877505415580-oikl39a5j38ff615rtpiejp1c00dsfok.apps.googleusercontent.com","project_id":"iconic-access-200414","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"UbJfTp7zh41KTIVKeO6fNhzi","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
       // Authorize a client with credentials, then call the Google Drive API.
@@ -49,7 +49,7 @@ module.exports = (app) => {
       function listEvents(auth) {
         const calendar = google.calendar({version: 'v3', auth});
         calendar.events.list({
-          calendarId: 'primary',
+          calendarId: 'brown.edu_rq0vojk0egcume8mkpbikruhjg@group.calendar.google.com',
           timeMin: (new Date()).toISOString(),
           maxResults: 10,
           singleEvents: true,
@@ -71,7 +71,151 @@ module.exports = (app) => {
         });
       }
   });
-  
+  app.get('/api/calendar/basketball', function(req, res){
+    // Load client secrets from a local file.
+      content = {"installed":{"client_id":"877505415580-oikl39a5j38ff615rtpiejp1c00dsfok.apps.googleusercontent.com","project_id":"iconic-access-200414","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"UbJfTp7zh41KTIVKeO6fNhzi","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
+      // Authorize a client with credentials, then call the Google Drive API.
+      authorize(content, listEvents);
+      /**
+       * Create an OAuth2 client with the given credentials, and then execute the
+       * given callback function.
+       * @param {Object} credentials The authorization client credentials.
+       * @param {function} callback The callback to call with the authorized client.
+       */
+      function authorize(credentials, callback) {
+        const {client_secret, client_id, redirect_uris} = credentials.installed;
+        const oAuth2Client = new OAuth2Client(client_id, client_secret, redirect_uris[0]);
+        console.log('2');
+        token = {"access_token":"ya29.GluWBbm1vW71sU9c5sZZykHjP6BEU5f1Pi8QgmD6wF6-LqEp3r1CNm1EGDFuZXAlCWgHFU1QxMJXpkjS-nlVNt4Q3CnDSSwX5t-mK1u5GxRRHZvME_2j5JMOh89m","token_type":"Bearer","refresh_token":"1/VwUGBtwuQuuJ8Ciqt8foZj2grSQd4VutrTX0ZbeTyac","expiry_date":1523116433466}
+          oAuth2Client.setCredentials(token);
+          callback(oAuth2Client);
+      }
+      /**
+       * Lists the next 10 events on the user's primary calendar.
+       * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+       */
+      function listEvents(auth) {
+        const calendar = google.calendar({version: 'v3', auth});
+        calendar.events.list({
+          calendarId: 'brown.edu_jk2qbab4b5msqg5o06bsv97arg@group.calendar.google.com',
+          timeMin: (new Date()).toISOString(),
+          maxResults: 10,
+          singleEvents: true,
+          orderBy: 'startTime',
+        }, (err, {data}) => {
+          if (err) return console.log('The API returned an error: ' + err);
+          const events = data.items;
+          if (events.length) {
+            console.log('Upcoming 10 events:');
+            events_send = [];
+            events_send = events.map((event, i) => {
+              const start = event.start.dateTime || event.start.date;
+              return(`${start} - ${event.summary}`)
+            });
+            res.send(events_send);
+          } else {
+            res.send('No upcoming events found.');
+          }
+        });
+      }
+  });
+  app.get('/api/calendar/nelson', function(req, res){
+    // Load client secrets from a local file.
+      content = {"installed":{"client_id":"877505415580-oikl39a5j38ff615rtpiejp1c00dsfok.apps.googleusercontent.com","project_id":"iconic-access-200414","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"UbJfTp7zh41KTIVKeO6fNhzi","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
+      // Authorize a client with credentials, then call the Google Drive API.
+      authorize(content, listEvents);
+      /**
+       * Create an OAuth2 client with the given credentials, and then execute the
+       * given callback function.
+       * @param {Object} credentials The authorization client credentials.
+       * @param {function} callback The callback to call with the authorized client.
+       */
+      function authorize(credentials, callback) {
+        const {client_secret, client_id, redirect_uris} = credentials.installed;
+        const oAuth2Client = new OAuth2Client(client_id, client_secret, redirect_uris[0]);
+        console.log('2');
+        token = {"access_token":"ya29.GluWBbm1vW71sU9c5sZZykHjP6BEU5f1Pi8QgmD6wF6-LqEp3r1CNm1EGDFuZXAlCWgHFU1QxMJXpkjS-nlVNt4Q3CnDSSwX5t-mK1u5GxRRHZvME_2j5JMOh89m","token_type":"Bearer","refresh_token":"1/VwUGBtwuQuuJ8Ciqt8foZj2grSQd4VutrTX0ZbeTyac","expiry_date":1523116433466}
+          oAuth2Client.setCredentials(token);
+          callback(oAuth2Client);
+      }
+      /**
+       * Lists the next 10 events on the user's primary calendar.
+       * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+       */
+      function listEvents(auth) {
+        const calendar = google.calendar({version: 'v3', auth});
+        calendar.events.list({
+          calendarId: 'brown.edu_7vo0v32566duu5fr3sllg2nt1k@group.calendar.google.com',
+          timeMin: (new Date()).toISOString(),
+          maxResults: 10,
+          singleEvents: true,
+          orderBy: 'startTime',
+        }, (err, {data}) => {
+          if (err) return console.log('The API returned an error: ' + err);
+          const events = data.items;
+          if (events.length) {
+            console.log('Upcoming 10 events:');
+            events_send = [];
+            events_send = events.map((event, i) => {
+              const start = event.start.dateTime || event.start.date;
+              return(`${start} - ${event.summary}`)
+            });
+            res.send(events_send);
+          } else {
+            res.send('No upcoming events found.');
+          }
+        });
+      }
+  });
+  app.get('/api/calendar/omac', function(req, res){
+    // Load client secrets from a local file.
+      content = {"installed":{"client_id":"877505415580-oikl39a5j38ff615rtpiejp1c00dsfok.apps.googleusercontent.com","project_id":"iconic-access-200414","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"UbJfTp7zh41KTIVKeO6fNhzi","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
+      // Authorize a client with credentials, then call the Google Drive API.
+      authorize(content, listEvents);
+      /**
+       * Create an OAuth2 client with the given credentials, and then execute the
+       * given callback function.
+       * @param {Object} credentials The authorization client credentials.
+       * @param {function} callback The callback to call with the authorized client.
+       */
+      function authorize(credentials, callback) {
+        const {client_secret, client_id, redirect_uris} = credentials.installed;
+        const oAuth2Client = new OAuth2Client(client_id, client_secret, redirect_uris[0]);
+        console.log('2');
+        token = {"access_token":"ya29.GluWBbm1vW71sU9c5sZZykHjP6BEU5f1Pi8QgmD6wF6-LqEp3r1CNm1EGDFuZXAlCWgHFU1QxMJXpkjS-nlVNt4Q3CnDSSwX5t-mK1u5GxRRHZvME_2j5JMOh89m","token_type":"Bearer","refresh_token":"1/VwUGBtwuQuuJ8Ciqt8foZj2grSQd4VutrTX0ZbeTyac","expiry_date":1523116433466}
+          oAuth2Client.setCredentials(token);
+          callback(oAuth2Client);
+      }
+      /**
+       * Lists the next 10 events on the user's primary calendar.
+       * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+       */
+      function listEvents(auth) {
+        const calendar = google.calendar({version: 'v3', auth});
+        calendar.events.list({
+          calendarId: 'brown.edu_1gklqf5558hsd1le5io9m7mnak@group.calendar.google.com',
+          timeMin: (new Date()).toISOString(),
+          maxResults: 10,
+          singleEvents: true,
+          orderBy: 'startTime',
+        }, (err, {data}) => {
+          if (err) return console.log('The API returned an error: ' + err);
+          const events = data.items;
+          if (events.length) {
+            console.log('Upcoming 10 events:');
+            events_send = [];
+            events_send = events.map((event, i) => {
+              const start = event.start.dateTime || event.start.date;
+              return(`${start} - ${event.summary}`)
+            });
+            res.send(events_send);
+          } else {
+            res.send('No upcoming events found.');
+          }
+        });
+      }
+  });
+
   app.put('/api/users/:userId', usersController.update);
   app.delete('/api/users/:userId', usersController.destroy);
 
